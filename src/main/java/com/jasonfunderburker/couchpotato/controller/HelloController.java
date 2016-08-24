48c3a172c69270d5377377dab003ffc5e7e19575
JasonFunderburker <ekaterina.bashkankova91@gmail.com>
2016-08-24 21:38:17 +0300

    init view template

--- src/main/java/com/jasonfunderburker/couchpotato/controller/HelloController.java
+++ src/main/java/com/jasonfunderburker/couchpotato/controller/HelloController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
@@ -16,10 +18,9 @@ public class HelloController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-        TorrentItem torrentItem = torrentItemDao.findById(1L);
+        List<TorrentItem> torrentItemsList = torrentItemDao.getItemsList();
 		model.addAttribute("message", "Hi, I'm couch potato, so i wrote this app cause i want to lay on my soft comfy coach and doing nothing when new episode of my favorite show is coming");
-        model.addAttribute("id", torrentItem.getId());
-        model.addAttribute("name", torrentItem.getName());
+        model.addAttribute("itemList", torrentItemsList);
 		return "hello";
 	}
 }
