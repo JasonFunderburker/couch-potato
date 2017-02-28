834058ca4e747fca2784bb56f1738e17b7ed46e0
EBashkankova <Ekaterina.Bashkankova@billing.ru>
2017-02-28 20:12:56 +0300

    store user info in db

---
+++ src/main/java/com/jasonfunderburker/couchpotato/DataSourceConfiguration.java
@@ -0,0 +1,29 @@
+package com.jasonfunderburker.couchpotato;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * Created on 28.02.2017
+ *
+ * @author JasonFunderburker
+ */
+@Configuration
+public class DataSourceConfiguration {
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String dataSourceDriverClass;
+
+    @Bean
+    public DataSource dataSource() {
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName(dataSourceDriverClass);
+        dataSource.setUrl(dataSourceUrl);
+        return dataSource;
+    }
+}
--- src/main/java/com/jasonfunderburker/couchpotato/SecurityConfiguration.java
+++ src/main/java/com/jasonfunderburker/couchpotato/SecurityConfiguration.java
@@ -1,12 +1,23 @@
 package com.jasonfunderburker.couchpotato;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by JasonFunderburker on 25.02.17.
@@ -21,22 +32,42 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Value("${couch-potato.password}")
     private String password;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated();
-        http
+                .authorizeRequests()
+                    .antMatchers("/css/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin().failureUrl("/login?error")
-                .defaultSuccessUrl("/")
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
-                .permitAll();
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/itemList")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(userName).password(password).roles("USER");
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setDataSource(dataSource);
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        auth.userDetailsService(userDetailsManager).passwordEncoder(encoder);
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+        if (!userDetailsManager.userExists(userName)) {
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("USER"));
+            User user = new User(userName, encoder.encode(password), authorities);
+
+            userDetailsManager.createUser(user);
+        }
     }
 }
